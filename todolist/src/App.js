fd92688542861a190c4f3cb399efc091b55c1bfe
Anna Savelyeva <knopka301299@mail.ru>
2019-09-26 20:16:26 +0300

    implemented checkboxes and listItems, added css

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from "./App.module.css";
-import {Input, Button} from "antd";
+import {Input, Button, Checkbox} from "antd";
 
 function App() {
+    const toDos = [1,2,3,4,5,6,7,8,9,10]
   return (
     <div className={styles.container}>
         <div className={styles.content}>
@@ -11,10 +12,18 @@ function App() {
             <Input/>
             <Button type="primary" className={styles.actionButton}> Add </Button>
             </div>
-
+            {toDos.map(()=> <ToDoItem/>)}
         </div>
     </div>
   );
 }
 
+function ToDoItem() {
+    return (<div className={styles.listContainer}>
+        <div className={styles.listItem}>
+            <Checkbox></Checkbox>
+            <h3 className={styles.todoTitle}> Buy cheese </h3>
+        </div>
+    </div>);
+}
 export default App;
